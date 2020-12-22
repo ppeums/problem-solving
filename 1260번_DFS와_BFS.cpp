6733aa0fb90141dfd0164ff189be5d5390981f46
GippeumLee <54873222+GippeumLee@users.noreply.github.com>
2020-12-22 17:15:04 +0900

    1260번 DFS와 BFS (2020-12-21)

---
+++ 1260번_DFS와_BFS.cpp
@@ -0,0 +1,66 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+
+#define MAX 1001
+
+int N, M, V, v1, v2;
+bool dvisit[MAX], bvisit[MAX];
+
+vector<int> map[MAX];
+queue<int> q;
+
+void DFS(int start) {
+	printf("%d ", start);
+	dvisit[start] = true;
+
+	for (int i = 0; i < map[start].size(); i++) {
+		int next = map[start][i];
+		if (!dvisit[next])
+			DFS(next);
+	}
+}
+
+void BFS(int start) {
+	q.push(start);
+	bvisit[start] = true;
+
+	while (!q.empty()) {
+		start = q.front();
+		printf("%d ", start);
+		q.pop();
+
+		for (int i = 0; i < map[start].size(); i++) {
+			int next = map[start][i];
+			if (!bvisit[next]) {
+				q.push(next);
+				bvisit[next] = true;
+			}
+		}
+	}
+}
+
+int main()
+{
+	scanf("%d %d %d", &N, &M, &V);
+
+	for (int i = 0; i < M; i++) {
+		scanf("%d %d", &v1, &v2);
+		map[v1].push_back(v2);
+		map[v2].push_back(v1);
+	}
+
+	for (int i = 1; i <= N; i++) {
+		sort(map[i].begin(), map[i].end());
+	}
+
+	DFS(V);
+	printf("\n");
+	BFS(V);
+	printf("\n");
+
+	return 0;
+}
