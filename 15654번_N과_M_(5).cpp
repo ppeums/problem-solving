dd8cf1c64745b0a67d9fb4c1e6b2298134d965f0
GippeumLee <54873222+GippeumLee@users.noreply.github.com>
2020-12-23 17:04:41 +0900

    15654번 N과 M (5) (2020-12-23)

---
+++ 15654번_N과_M_(5).cpp
@@ -0,0 +1,44 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX 9
+
+int N, M;
+int input[MAX], ans[MAX];
+bool visit[MAX];
+
+void DFS(int cnt) {
+	if (cnt == M) {
+		for (int i = 0; i < M; i++) {
+			printf("%d ", ans[i]);
+		}
+		printf("\n");
+		return;
+	}
+
+	for (int i = 0; i < N; i++) {
+		if (!visit[i]) {
+			ans[cnt] = input[i];
+			visit[i] = true;
+			DFS(cnt + 1);
+			visit[i] = false;
+		}
+	}
+}
+
+int main()
+{
+	scanf("%d %d", &N, &M);
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &input[i]);
+	}
+
+	sort(input, input + N);
+
+	DFS(0);
+
+	return 0;
+}
