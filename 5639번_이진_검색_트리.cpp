b06c28f850edc0f198729589b80fb789b13e4554
GippeumLee <54873222+GippeumLee@users.noreply.github.com>
2020-12-29 17:07:57 +0900

    5639번 이진 검색 트리 (2020-12-29)

---
+++ 5639번_이진_검색_트리.cpp
@@ -0,0 +1,49 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+struct node {
+	node* left;
+	node* right;
+	int value;
+};
+
+node* root;
+
+node* insert(node* root, int n) {
+	if (root == nullptr) {
+		root = new node();
+		root->left = root->right = nullptr;
+		root->value = n;
+	}
+	else if (n < root->value) {
+		root->left = insert(root->left, n);
+	}
+	else if (n > root->value) {
+		root->right = insert(root->right, n);
+	}
+	return root;
+}
+
+void postorder(node* root) {
+	if (root != nullptr) {
+		postorder(root->left);
+		postorder(root->right);
+		printf("%d\n", root->value);
+	}
+}
+
+int main()
+{
+	int n;
+
+	root = nullptr;
+
+	while (scanf("%d", &n) != EOF) {
+		root = insert(root, n);
+	}
+
+	postorder(root);
+
+	return 0;
+}
