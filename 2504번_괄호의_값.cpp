76d9538107443cdd62c62611e69e83ec55d0c3b3
GippeumLee <dlrlqma006@naver.com>
2021-01-23 17:33:51 +0900

    2504번 괄호의 값 (2021-01-23)

---
+++ 2504번_괄호의_값.cpp
@@ -0,0 +1,59 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main()
+{	
+	string input;
+	stack<char> s;
+	int tmp = 1, ans = 0;
+	bool isWrong = false;
+
+	cin >> input;
+
+	int len = input.length();
+
+	for (int i = 0; i < len; i++) {
+		if (input[i] == '(') {
+			s.push(input[i]);
+			tmp *= 2;
+		}
+		else if (input[i] == '[') {
+			s.push(input[i]);
+			tmp *= 3;
+		}
+		else if (input[i] == ')') {
+			if (s.empty() || s.top() != '(') {
+				isWrong = true;
+				break;
+			}
+			if (s.top() == '(') {
+				if (input[i - 1] == '(')
+					ans += tmp;
+				s.pop();
+				tmp /= 2;
+			}
+		}
+		else if (input[i] == ']') {
+			if (s.empty() || s.top() != '[') {
+				isWrong = true;
+				break;
+			}
+			if (s.top() == '[') {
+				if (input[i - 1] == '[')
+					ans += tmp;
+				s.pop();
+				tmp /= 3;
+			}
+		}
+	}
+
+	if (isWrong || !s.empty())
+		printf("0\n");
+	else
+		printf("%d\n", ans);
+	
+	return 0;
+}
