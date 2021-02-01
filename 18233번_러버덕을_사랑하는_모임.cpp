5779fff9ea9502040ddea158e335969a886c5eac
GippeumLee <dlrlqma006@naver.com>
2021-02-01 23:53:38 +0900

    18233번 러버덕을 사랑하는 모임 (2021-02-01)

---
+++ 18233번_러버덕을_사랑하는_모임.cpp
@@ -0,0 +1,62 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+#define MAX 21
+
+int N, P, E;
+int man[MAX], doll[MAX];
+bool isAnswer = false;
+pair<int, int> input[MAX];
+
+void DFS(int start, int cnt) {
+	if (isAnswer) return;
+	if (cnt == P) {
+		memset(doll, 0, sizeof(doll));
+		int smin = 0, smax = 0;
+		for (int i = 0; i < P; i++) {
+			int idx = man[i];
+			doll[idx] = input[idx].first;
+			smin += input[idx].first;
+			smax += input[idx].second;
+		}
+		if (E >= smin && E <= smax) {
+			for (int i = 1; i <= N; i++) {
+				if (smin == E) {
+					isAnswer = true;
+					break;
+				}
+				if (doll[i] > 0 && doll[i] < input[i].second) {
+					doll[i]++, smin++;
+				}
+			}
+		}
+		if (isAnswer) {
+			for (int i = 1; i <= N; i++) {
+				printf("%d ", doll[i]);
+			}
+			printf("\n");
+		}
+		return;
+	}
+	for (int i = start; i <= N; i++) {
+		man[cnt] = i;
+		DFS(i + 1, cnt + 1);
+	}
+}
+
+int main()
+{	
+	scanf("%d %d %d", &N, &P, &E);
+
+	for (int i = 1; i <= N; i++) {
+		scanf("%d %d", &input[i].first, &input[i].second);
+	}
+
+	DFS(1, 0);
+
+	if (!isAnswer) printf("-1\n");
+
+	return 0;
+}
