f56b909f52212237ce5731928b6cdfd02d3ef958
ppeums <dlrlqma006@naver.com>
2021-03-10 11:46:22 +0900

    19582번 200년간 폐관수련했더니 PS 최강자가 된 건에 대하여 (2021-03-10)

---
+++ 19582번_200년간_폐관수련했더니_PS_최강자가_된_건에_대하여.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+#define MAX 100001
+
+int N, X, P, maxi, pass = 1;
+long long sum;
+
+int main()
+{
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		scanf("%d %d", &X, &P);
+		if (sum <= X) {
+			sum += P;
+			maxi = (maxi < P) ? P : maxi;
+		}
+		else if (sum - maxi > X || maxi < P) {
+			pass--;
+		}
+		else {
+			pass--;
+			sum -= maxi;
+			sum += P;
+		}
+		if (pass < 0) {
+			printf("Zzz\n");
+			return 0;
+		}
+	}
+	printf("Kkeo-eok\n");
+	return 0;
+}
