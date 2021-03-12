477b395d1464b262706e33f748c8e6ca6fede96c
ppeums <dlrlqma006@naver.com>
2021-03-12 10:25:17 +0900

    16562번 친구비 (2021-03-12)

--- 16562번_친구비.cpp
+++ 16562번_친구비.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 #define MAX 10001
 
-int N, M, K;
+int N, M, K, sum;
 int pay[MAX], parent[MAX];
 bool check[MAX];
 
@@ -27,28 +27,22 @@ int Find(int a) {
 void Union(int a, int b) {
 	a = Find(a);
 	b = Find(b);
-	if (pay[a] < pay[b])
-		parent[b] = a;
+	if (pay[a] < pay[b]) parent[b] = a;
 	else parent[a] = b;
 }
 
 int main()
 {
 	scanf("%d %d %d", &N, &M, &K);
-
 	for (int i = 1; i <= N; i++) {
 		scanf("%d", &pay[i]);
 	}
-
 	Init();
-
 	for (int i = 0; i < M; i++) {
 		int v, w;
 		scanf("%d %d", &v, &w);
 		Union(v, w);
 	}
-
-	int sum = 0;
 	for (int i = 1; i <= N; i++) {
 		int p = Find(i);
 		if (!check[p]) {
@@ -56,11 +50,7 @@ int main()
 			sum += pay[p];
 		}
 	}
-
-	if (sum <= K) 
-		printf("%d\n", sum);
-	else 
-		printf("Oh no\n");
-
+	if (sum <= K) printf("%d\n", sum);
+	else printf("Oh no\n");
 	return 0;
 }
