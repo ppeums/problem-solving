8ab24ffd24c2122ca001e205617b468ab99a0dc7
ppeums <dlrlqma006@naver.com>
2021-03-12 10:25:59 +0900

    2003번 수들의 합 2 (2021-03-12)

---
+++ 2003번_수들의_합_2.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+#define MAX 10001
+
+int N, M, sum, ans, start;
+int arr[MAX];
+
+int main()
+{
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &arr[i]);
+	}
+	for (int i = 0; i < N; i++) {
+		sum += arr[i];
+		if (sum >= M) {
+			while (sum >= M) {
+				if (sum == M)
+					ans++;
+				sum -= arr[start];
+				start++;
+			}
+		}
+	}
+	printf("%d\n", ans);
+	return 0;
+}
