8b08e6a3ff0319e3675d0d0556d64a51a3d33b1d
ppeums <dlrlqma006@naver.com>
2021-03-15 13:22:27 +0900

    11478번 서로 다른 부분 문자열의 개수 (2021-03-15)

---
+++ 11478번_서로_다른_부분_문자열의_개수.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <set>
+using namespace std;
+
+string str;
+set<string> s;
+
+int main()
+{
+	cin >> str;
+	int len = str.length();	
+	for (int i = 0; i < len; i++) {
+		string tmp = "";
+		for (int j = i; j < len; j++) {
+			tmp += str[j];
+			s.insert(tmp);
+		}
+	}
+	cout << s.size() << endl;	
+	return 0;
+}
