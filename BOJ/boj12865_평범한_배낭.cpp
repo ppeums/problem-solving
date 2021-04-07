1c6e22d364858d3e288a822ae92f741a8ecba71e
ppeums <dlrlqma006@naver.com>
2021-04-07 16:25:33 +0900

    boj12865 평범한 배낭 (2021-04-07)

---
+++ BOJ/boj12865_평범한_배낭.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define NMAX 101
+#define KMAX 100001
+
+int N, K;
+int w[NMAX], v[NMAX], d[NMAX][KMAX];
+
+int main() {
+	scanf("%d %d", &N, &K);
+	for (int i = 1; i <= N; i++) {
+		scanf("%d %d", &w[i], &v[i]);
+
+	}
+	for (int i = 1; i <= N; i++) {
+		for (int j = 0; j <= K; j++) {
+			if (j < w[i]) d[i][j] = d[i - 1][j];
+			else d[i][j] = max(d[i - 1][j - w[i]] + v[i], d[i - 1][j]);
+		}
+	}
+	printf("%d\n", d[N][K]);
+	return 0;
+}
