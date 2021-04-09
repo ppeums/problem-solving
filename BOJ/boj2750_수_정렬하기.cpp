b093f3e6d904e96e27e521bc7d3ba995f94c7266
ppeums <dlrlqma006@naver.com>
2021-04-09 16:19:41 +0900

    boj2750 수 정렬하기 (2021-04-09)

---
+++ BOJ/boj2750_수_정렬하기.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX 1001
+
+int N, arr[MAX];
+
+int main() {
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &arr[i]);
+	}
+	sort(arr, arr + N);
+	for (int i = 0; i < N; i++) {
+		printf("%d\n", arr[i]);
+	}
+	return 0;
+}
