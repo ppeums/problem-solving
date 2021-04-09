f93256be517e81ae384935b9eaf43feaa07b580f
ppeums <dlrlqma006@naver.com>
2021-04-09 16:03:16 +0900

    boj1920 수 찾기 (2021-04-09)

---
+++ BOJ/boj1920_수_찾기.cpp
@@ -0,0 +1,39 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX 100001
+
+int N, M;
+int a[MAX], b[MAX];
+
+void binarySearch(int num) {
+	int left = 0, right = N - 1;
+	while (left <= right) {
+		int mid = (left + right) / 2;
+		if (num == a[mid]) {
+			printf("1\n");
+			return;
+		}
+		else if (num < a[mid]) right = mid - 1;
+		else left = mid + 1;
+	}
+	printf("0\n");
+}
+
+int main() {
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &a[i]);
+	}
+	scanf("%d", &M);
+	for (int i = 0; i < M; i++) {
+		scanf("%d", &b[i]);
+	}
+	sort(a, a + N);
+	for (int i = 0; i < M; i++) {
+		binarySearch(b[i]);
+	}	
+	return 0;
+}
