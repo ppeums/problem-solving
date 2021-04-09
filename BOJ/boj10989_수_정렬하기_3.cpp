141088fadb9cb9c766ca93b89746feb5dc9aaa37
ppeums <dlrlqma006@naver.com>
2021-04-09 16:30:57 +0900

    boj10989 수 정렬하기 3 (2021-04-09)

---
+++ BOJ/boj10989_수_정렬하기_3.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX 10001
+
+int N, maxi, cnt[MAX];
+bool check[MAX];
+
+int main() {
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		int num;
+		scanf("%d", &num);
+		cnt[num]++;
+		check[num] = true;
+		maxi = max(maxi, num);
+	}
+	for (int i = 1; i <= maxi; i++) {
+		if (check[i]) {
+			for (int j = 0; j < cnt[i]; j++) {
+				printf("%d\n", i);
+			}
+		}
+	}
+	return 0;
+}
