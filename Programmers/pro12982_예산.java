b986ca72ac0645c59edf32227279d1f005f5407f
ppeums <dlrlqma006@naver.com>
2021-04-12 14:25:57 +0900

    pro12982 예산 (2021-04-12)

---
+++ Programmers/pro12982_예산.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        for (int i = 0; i < d.length; i++) {
+            if (budget >= d[i]) {
+                answer++;
+                budget -= d[i];
+            }
+        }
+        return answer;
+    }
+}
