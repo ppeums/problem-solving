0d045b7e46bf425afb20765ffbc827f815d72cbc
ppeums <dlrlqma006@naver.com>
2021-04-18 21:23:20 +0900

    boj10870 피보나치 수 5 (2021-04-18)

---
+++ BOJ/boj10870_피보나치_수_5.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] f = new int[21];
+        f[0] = 0;
+        f[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+        bw.write(f[n] + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
