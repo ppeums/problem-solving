c6c1ae08cdc5c60a5a079709477a2dd3b97c847b
ppeums <dlrlqma006@naver.com>
2021-04-20 15:41:17 +0900

    Resolve pro42576 완주하지 못한 선수 (2021-04-20)

--- Programmers/pro42576_완주하지_못한_선수.java
+++ Programmers/pro42576_완주하지_못한_선수.java
@@ -1,25 +1,20 @@
 import java.util.HashMap;
-import java.util.Map;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        Map<String, Integer> hm = new HashMap<>();
-        for (String p : participant) {
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String p : participant)
             hm.put(p, 0);
-        }
-        for (String p : participant) {
+        for (String p : participant)
             hm.put(p, hm.get(p) + 1);
-        }
-        for (String c : completion) {
+        for (String c : completion)
             hm.put(c, hm.get(c) - 1);
-        }
-        for (String p : participant) {
-            if (hm.get(p) == 1) {
+        for (String p : participant)
+            if (hm.get(p) != 0) {
                 answer = p;
                 break;
             }
-        }
         return answer;
     }
 }
