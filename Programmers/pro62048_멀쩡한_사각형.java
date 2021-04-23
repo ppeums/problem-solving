422de28793568b4f2f33b14b18e20fa9e817152c
ppeums <dlrlqma006@naver.com>
2021-04-23 17:04:17 +0900

    Resolve pro62048 멀쩡한 사각형 (2021-04-23)

--- Programmers/pro62048_멀쩡한_사각형.java
+++ Programmers/pro62048_멀쩡한_사각형.java
@@ -2,8 +2,9 @@ import java.math.BigInteger;
 
 class Solution {
     public long solution(int w, int h) {
-        long gcd = BigInteger.valueOf(w).gcd(BigInteger.valueOf(h)).longValue();
-        long answer = ((long) w * (long) h) - ((long) w + (long) h - gcd);
-        return answer;
+        BigInteger b1 = BigInteger.valueOf(w);
+        BigInteger b2 = BigInteger.valueOf(h);
+        long gcd = b1.gcd(b2).longValue();
+        return ((long) w * (long) h) - (w + h - gcd);
     }
 }
