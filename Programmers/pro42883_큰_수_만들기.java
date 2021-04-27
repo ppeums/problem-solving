bab5bbdb708770bf42dfd26e8d39aaf69907e112
ppeums <dlrlqma006@naver.com>
2021-04-27 15:47:48 +0900

    Resolve pro42883 큰 수 만들기 (2021-04-27)

--- Programmers/pro42883_큰_수_만들기.java
+++ Programmers/pro42883_큰_수_만들기.java
@@ -1,17 +1,23 @@
+import java.util.Stack;
+
 class Solution {
     public String solution(String number, int k) {
-        StringBuilder sb = new StringBuilder(number);
-        int i = 1;
-        while (true) {
-            if (k <= 0 || i >= sb.length()) break;
-            if (sb.charAt(i - 1) < sb.charAt(i)) {
-                sb.deleteCharAt(i - 1);
-                k--;
-                if (i > 1) i--;
-            } else i++;
+        String answer = "";
+        int len = number.length() - k;
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < number.length(); i++) {
+            char ch = number.charAt(i);
+            if (!st.isEmpty() && st.peek() < ch && k > 0) {
+                while (true) {
+                    if (st.isEmpty() || st.peek() >= ch || k == 0) break;
+                    st.pop();
+                    k--;
+                }
+            }
+            st.add(ch);
         }
-        if (k > 0)
-            sb.delete(sb.length() - k, sb.length());
-        return sb.toString();
+        while (!st.isEmpty())
+            answer = st.pop() + answer;
+        return answer.substring(0, len);
     }
 }
