1e280823663edc5259a309ef0283ca5fafe56990
ppeums <dlrlqma006@naver.com>
2021-04-28 22:07:47 +0900

    Resolve pro42746 가장 큰 수 (2021-04-28)

--- Programmers/pro42746_가장_큰_수.java
+++ Programmers/pro42746_가장_큰_수.java
@@ -3,20 +3,23 @@ import java.util.Comparator;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] s = new String[numbers.length];
+        String answer = "";
+        String[] str = new String[numbers.length];
         for (int i = 0; i < numbers.length; i++)
-            s[i] = Integer.toString(numbers[i]);
-        Arrays.sort(s, new Comparator<String>() {
+            str[i] = Integer.toString(numbers[i]);
+        Arrays.sort(str, new Comparator<String>() {
             @Override
-            public int compare(String o1, String o2) {
-                if (Integer.parseInt(o1 + o2) < Integer.parseInt(o2 + o1))
+            public int compare(String s1, String s2) {
+                if (Integer.parseInt(s1 + s2) < Integer.parseInt(s2 + s1))
                     return 1;
-                else if (Integer.parseInt(o1 + o2) > Integer.parseInt(o2 + o1))
+                else if (Integer.parseInt(s1 + s2) > Integer.parseInt(s2 + s1))
                     return -1;
-                else return 0;
+                else
+                    return 0;
             }
         });
-        String answer = Arrays.toString(s).replaceAll("[^0-9]", "");
+        for (int i = 0; i < str.length; i++)
+            answer += str[i];
         while (!answer.equals("0") && answer.startsWith("0"))
             answer = answer.substring(1, answer.length());
         return answer;
