4a39c5cf2034304c661ce24efb01abdba36e4231
ppeums <dlrlqma006@naver.com>
2021-05-02 22:28:17 +0900

    pro12980 점프와 순간 이동 (2021-05-02)

---
+++ Programmers/pro12980_점프와_순간_이동.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int ans = 0;
+        while (n > 0) {
+            if (n % 2 != 0) ans++;
+            n /= 2;
+        }
+        return ans;
+    }
+}
