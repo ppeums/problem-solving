30f65bc75ea18dc3357cef1b3f9873c2a7f3ca02
ppeums <dlrlqma006@naver.com>
2021-05-05 22:17:57 +0900

    pro68936 쿼드압축 후 개수 세기 (2021-05-05)

---
+++ Programmers/pro68936_쿼드압축_후_개수_세기.java
@@ -0,0 +1,31 @@
+class Solution {
+    static int[] answer = new int[2];
+    static int[][] map;
+
+    public int[] solution(int[][] arr) {
+        map = arr;
+        func(0, 0, arr.length);
+        return answer;
+    }
+
+    static void func(int y, int x, int n) {
+        boolean isSame = true;
+        for (int i = y; i < y + n; i++) {
+            for (int j = x; j < x + n; j++) {
+                if (map[i][j] != map[y][x]) {
+                    isSame = false;
+                    break;
+                }
+            }
+        }
+        if (isSame)
+            answer[map[y][x]]++;
+        else {
+            n /= 2;
+            func(y, x, n);
+            func(y + n, x, n);
+            func(y, x + n, n);
+            func(y + n, x + n, n);
+        }
+    }
+}
