f5ec08516ea029f1e90d43f934af8c17ee1c5ee6
ppeums <dlrlqma006@naver.com>
2021-05-06 23:06:56 +0900

    Resolve pro12941 최솟값 만들기 (2021-05-06)

--- Programmers/pro12941_최솟값_만들기.java
+++ Programmers/pro12941_최솟값_만들기.java
@@ -2,11 +2,11 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] A, int[] B) {
-        int answer = 0;
+        int answer = 0, len = A.length;
         Arrays.sort(A);
         Arrays.sort(B);
-        for (int i = 0; i < A.length; i++)
-            answer += A[i] * B[A.length - i - 1];
+        for (int i = 0; i < len; i++)
+            answer += A[i] * B[len - 1 - i];
         return answer;
     }
 }
