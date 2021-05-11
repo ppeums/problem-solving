5871023c5eb44dc8ee30afeb5d6f8d93d7e196ec
ppeums <dlrlqma006@naver.com>
2021-05-11 21:56:22 +0900

    Resolve pro42584 주식가격 (2021-05-11)

--- Programmers/pro42584_주식가격.java
+++ Programmers/pro42584_주식가격.java
@@ -3,18 +3,19 @@ import java.util.Stack;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        Stack<Integer> st = new Stack<>();
-        st.add(0);
+        Stack<Integer> stack = new Stack<>();
+        stack.add(0);
         for (int i = 1; i < prices.length; i++) {
-            while (!st.isEmpty() && prices[st.peek()] > prices[i]) {
-                int top = st.pop();
+            while (true) {
+                if (stack.isEmpty() || prices[stack.peek()] <= prices[i]) break;
+                int top = stack.pop();
                 answer[top] = i - top;
             }
-            st.add(i);
+            stack.add(i);
         }
-        while (!st.isEmpty()) {
-            int top = st.pop();
-            answer[top] = answer.length - 1 - top;
+        while (!stack.isEmpty()) {
+            int top = stack.pop();
+            answer[top] = prices.length - 1 - top;
         }
         return answer;
     }
