49a875b2a6f9c4918f62f144d1ddec25c6bc8b6e
ppeums <dlrlqma006@naver.com>
2021-05-17 23:17:19 +0900

    Resolve pro12978 배달 (2021-05-17)

--- Programmers/pro12978_배달.java
+++ Programmers/pro12978_배달.java
@@ -3,26 +3,23 @@ import java.util.Arrays;
 class Solution {
     static int n;
     static int[] dist;
-    static int[][] cost;
+    static int[][] time;
     static boolean[] visit;
-    static boolean[][] link;
 
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
         n = N;
         dist = new int[N + 1];
-        cost = new int[N + 1][N + 1];
+        time = new int[N + 1][N + 1];
         visit = new boolean[N + 1];
-        link = new boolean[N + 1][N + 1];
-        for (int i = 0; i < road.length; i++) {
-            int start = road[i][0], end = road[i][1], time = road[i][2];
-            link[start][end] = link[end][start] = true;
-            cost[start][end] = cost[end][start] = (cost[start][end] == 0) ? time : Math.min(time, cost[start][end]);
+        for (int[] r : road) {
+            time[r[0]][r[1]] = time[r[1]][r[0]] = (time[r[0]][r[1]] == 0) ? r[2] : Math.min(r[2], time[r[0]][r[1]]);
         }
         dijkstra();
         for (int i = 1; i <= N; i++) {
-            if (dist[i] <= K)
+            if (dist[i] <= K) {
                 answer++;
+            }
         }
         return answer;
     }
@@ -32,22 +29,23 @@ class Solution {
         dist[1] = 0;
         visit[1] = true;
         for (int i = 1; i <= n; i++) {
-            if (link[1][i])
-                dist[i] = cost[1][i];
+            if (time[1][i] > 0) {
+                dist[i] = time[1][i];
+            }
         }
         for (int i = 0; i < n - 1; i++) {
             int min = Integer.MAX_VALUE;
             int idx = -1;
             for (int j = 1; j <= n; j++) {
-                if (!visit[j] && dist[j] < min) {
+                if (!visit[j] && min > dist[j]) {
                     min = dist[j];
                     idx = j;
                 }
             }
             visit[idx] = true;
             for (int j = 1; j <= n; j++) {
-                if (!visit[j] && link[idx][j] && dist[j] > dist[idx] + cost[idx][j]) {
-                    dist[j] = dist[idx] + cost[idx][j];
+                if (!visit[j] && time[idx][j] > 0 && dist[j] > dist[idx] + time[idx][j]) {
+                    dist[j] = dist[idx] + time[idx][j];
                 }
             }
         }
