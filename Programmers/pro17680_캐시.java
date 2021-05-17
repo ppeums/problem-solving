35248600d57c7a84543e86baeaa5bfa41e46c368
ppeums <dlrlqma006@naver.com>
2021-05-17 23:16:54 +0900

    Resolve pro17680 캐시 (2021-05-17)

--- Programmers/pro17680_캐시.java
+++ Programmers/pro17680_캐시.java
@@ -5,19 +5,17 @@ class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
         Queue<String> q = new LinkedList<>();
-        if (cacheSize == 0)
-            return cities.length * 5;
         for (int i = 0; i < cities.length; i++) {
             String city = cities[i].toLowerCase();
-            if (q.contains(city)) {
-                answer += 1;
-                q.remove(city);
+            if (q.remove(city)) {
+                answer++;
             } else {
-                if (q.size() == cacheSize)
-                    q.poll();
                 answer += 5;
             }
             q.add(city);
+            if (q.size() > cacheSize) {
+                q.poll();
+            }
         }
         return answer;
     }
