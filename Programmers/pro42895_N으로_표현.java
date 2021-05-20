1b2d8218472fd42df6832510a465df08a80560ef
ppeums <dlrlqma006@naver.com>
2021-05-20 23:06:48 +0900

    Resolve pro42895 N으로 표현 (2021-05-20)

--- Programmers/pro42895_N으로_표현.java
+++ Programmers/pro42895_N으로_표현.java
@@ -1,32 +1,24 @@
-import java.util.LinkedHashSet;
-import java.util.Iterator;
+import java.util.HashSet;
 import java.util.Set;
 
 class Solution {
     public int solution(int N, int number) {
         Set<Integer>[] set = new Set[9];
         for (int i = 1; i <= 8; i++) {
-            set[i] = new LinkedHashSet<>();
-            String sn = Integer.toString(N);
-            String str = "";
-            for (int j = 1; j <= i; j++) {
-                str += sn;
+            set[i] = new HashSet<>();
+            int num = 0;
+            for (int j = 0; j < i; j++) {
+                num = num * 10 + N;
             }
-            set[i].add(Integer.parseInt(str));
-        }
-        for (int i = 1; i <= 8; i++) {
-            for (int j = 1; j <= i - 1; j++) {
-                Iterator<Integer> it = set[j].iterator();
-                while (it.hasNext()) {
-                    int n1 = it.next();
-                    Iterator<Integer> it2 = set[i - j].iterator();
-                    while (it2.hasNext()) {
-                        int n2 = it2.next();
-                        set[i].add(n1 + n2);
-                        set[i].add(n1 - n2);
-                        set[i].add(n1 * n2);
-                        if (n2 != 0) {
-                            set[i].add(n1 / n2);
+            set[i].add(num);
+            for (int j = 1; j < i; j++) {
+                for (Integer in : set[j]) {
+                    for (Integer in2 : set[i - j]) {
+                        set[i].add(in + in2);
+                        set[i].add(in - in2);
+                        set[i].add(in * in2);
+                        if (in2 != 0) {
+                            set[i].add(in / in2);
                         }
                     }
                 }
