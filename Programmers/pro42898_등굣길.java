aac8904164c344849a84df14aefa5627c197ef4c
ppeums <dlrlqma006@naver.com>
2021-05-27 23:05:44 +0900

    Resolve pro42898 등굣길 (2021-05-27)

--- Programmers/pro42898_등굣길.java
+++ Programmers/pro42898_등굣길.java
@@ -2,20 +2,24 @@ class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int[][] map = new int[n + 1][m + 1];
         boolean[][] isPuddle = new boolean[n + 1][m + 1];
-        for (int[] p : puddles) {
-            isPuddle[p[1]][p[0]] = true;
+        for (int i = 0; i < puddles.length; i++) {
+            isPuddle[puddles[i][1]][puddles[i][0]] = true;
         }
         map[1][1] = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
+        for (int i = 2; i <= m; i++) {
+            if (!isPuddle[1][i]) {
+                map[1][i] = map[1][i - 1];
+            }
+        }
+        for (int i = 2; i <= n; i++) {
+            if (!isPuddle[i][1]) {
+                map[i][1] = map[i - 1][1];
+            }
+        }
+        for (int i = 2; i <= n; i++) {
+            for (int j = 2; j <= m; j++) {
                 if (!isPuddle[i][j]) {
-                    if (i > 1) {
-                        map[i][j] += map[i - 1][j];
-                    }
-                    if (j > 1) {
-                        map[i][j] += map[i][j - 1];
-                    }
-                    map[i][j] %= 1000000007;
+                    map[i][j] = (map[i - 1][j] + map[i][j - 1]) % 1000000007;
                 }
             }
         }
