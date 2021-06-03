bbb2f5ef77a4a6ec892e0c9375c9d6325f3f9943
ppeums <dlrlqma006@naver.com>
2021-06-03 22:18:01 +0900

    pro12914 멀리 뛰기 (2021-06-03)

---
+++ Programmers/pro12914_멀리_뛰기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long solution(int n) {
+        long[] d = new long[2001];
+        int mod = 1234567;
+        d[1] = 1;
+        d[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            d[i] = (d[i - 1] + d[i - 2]) % mod;
+        }
+        return d[n];
+    }
+}
