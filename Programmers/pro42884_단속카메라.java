79ba2c92fdca4d72d625fae991e56f1503f0ee6b
ppeums <dlrlqma006@naver.com>
2021-06-05 22:30:05 +0900

    Resolve pro42884 단속카메라 (2021-06-05)

--- Programmers/pro42884_단속카메라.java
+++ Programmers/pro42884_단속카메라.java
@@ -1,17 +1,11 @@
 import java.util.Arrays;
-import java.util.Comparator;
 
 class Solution {
     public int solution(int[][] routes) {
-        Arrays.sort(routes, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return Integer.compare(o1[1], o2[1]);
-            }
-        });
+        Arrays.sort(routes, (o1, o2) -> Integer.compare(o1[1], o2[1]));
         int answer = 0, min = -30001;
         for (int[] route : routes) {
-            if (min < route[0]) {
+            if (route[0] > min) {
                 answer++;
                 min = route[1];
             }
