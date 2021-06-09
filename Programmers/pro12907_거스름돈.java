1cd86cd28984df2bc41465e314c76946ef7b2d50
ppeums <dlrlqma006@naver.com>
2021-06-09 22:08:48 +0900

    Resolve pro12907 거스름돈 (2021-06-09)

--- Programmers/pro12907_거스름돈.java
+++ Programmers/pro12907_거스름돈.java
@@ -1,18 +1,15 @@
 class Solution {
     public int solution(int n, int[] money) {
         int[][] d = new int[money.length + 1][n + 1];
-        int mod = 1000000007;
         for (int i = 1; i <= money.length; i++) {
             for (int j = 0; j <= n; j++) {
                 if (j == 0) {
                     d[i][j] = 1;
                 } else {
+                    d[i][j] = d[i - 1][j];
                     if (j - money[i - 1] >= 0) {
-                        d[i][j] = d[i - 1][j] + d[i][j - money[i - 1]];
-                    } else {
-                        d[i][j] = d[i - 1][j];
+                        d[i][j] += d[i][j - money[i - 1]];
                     }
-                    d[i][j] %= mod;
                 }
             }
         }
