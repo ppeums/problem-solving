c4198c5a24f3c82402fbffb39ef7a05c5a21ff6d
ppeums <dlrlqma006@naver.com>
2021-06-26 22:28:46 +0900

    Resolve pro70130 스타 수열 (2021-06-26)

--- Programmers/pro70130_스타_수열.java
+++ Programmers/pro70130_스타_수열.java
@@ -1,17 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int solution(int[] a) {
         int answer = 0;
-        int[] count = new int[a.length];
+        Map<Integer, Integer> map = new HashMap<>();
+        ArrayList<int[]> list = new ArrayList<>();
         for (int i = 0; i < a.length; i++) {
-            count[a[i]]++;
+            map.put(a[i], map.getOrDefault(a[i], 0) + 1);
         }
-        for (int i = 0; i < count.length; i++) {
-            if (count[i] * 2 <= answer) {
-                continue;
+        for (int key : map.keySet()) {
+            list.add(new int[]{key, map.get(key)});
+        }
+        Collections.sort(list, (o1, o2) -> Integer.compare(o2[1], o1[1]));
+        for (int i = 0; i < list.size(); i++) {
+            int star = list.get(i)[0];
+            if (list.get(i)[1] * 2 <= answer) {
+                break;
             }
             int tmp = 0;
             for (int j = 0; j < a.length - 1; j++) {
-                if ((a[j] == i || a[j + 1] == i) && (a[j] != a[j + 1])) {
+                if (a[j] != a[j + 1] && (a[j] == star || a[j + 1] == star)) {
                     tmp += 2;
                     j++;
                 }
