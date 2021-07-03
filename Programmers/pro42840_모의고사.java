29a5ec3130b0948de4c9bcc014d14c28860d5d91
ppeums <dlrlqma006@naver.com>
2021-07-03 23:33:55 +0900

    Resolve pro42840 모의고사 (2021-07-03)

--- Programmers/pro42840_모의고사.java
+++ Programmers/pro42840_모의고사.java
@@ -1,30 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public int[] solution(int[] answers) {
-        int[][] p = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
-        int[] sum = new int[3];
-        int[] idx = new int[3];
-        int[] lim = {5, 8, 10};
-        int max = 0, cnt = 0;
+        int[][] person = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+        int[] score = new int[3];
+        int maxScore = 0;
         for (int i = 0; i < answers.length; i++) {
             for (int j = 0; j < 3; j++) {
-                if (answers[i] == p[j][idx[j]++]) {
-                    sum[j]++;
-                    max = Math.max(max, sum[j]);
+                if (answers[i] == person[j][i % person[j].length]) {
+                    score[j]++;
+                    maxScore = Math.max(maxScore, score[j]);
                 }
-                if (idx[j] == lim[j])
-                    idx[j] = 0;
             }
         }
+        List<Integer> answer = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
-            if (max == sum[i])
-                cnt++;
+            if (score[i] == maxScore) {
+                answer.add(i + 1);
+            }
         }
-        int[] answer = new int[cnt];
-        int j = 0;
-        for (int i = 0; i < 3; i++) {
-            if (max == sum[i])
-                answer[j++] = i + 1;
-        }
-        return answer;
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
