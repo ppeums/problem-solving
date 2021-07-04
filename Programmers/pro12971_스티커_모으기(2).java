cd6cb9a410f3549842ee3123d79ab58ccbc1ef04
ppeums <dlrlqma006@naver.com>
2021-07-04 23:06:19 +0900

    Resolve pro12971 스티커 모으기(2) (2021-07-04)

--- Programmers/pro12971_스티커_모으기(2).java
+++ Programmers/pro12971_스티커_모으기(2).java
@@ -13,9 +13,11 @@ class Solution {
         d2[0] = 0;
         d2[1] = sticker[1];
         for (int i = 2; i < n - 1; i++) {
-            d1[i] = Math.max(d1[i - 2] + sticker[i], d1[i - 1]);
-            d2[i] = Math.max(d2[i - 2] + sticker[i], d2[i - 1]);
+            d1[i] = Math.max(d1[i - 1], d1[i - 2] + sticker[i]);
+            d2[i] = Math.max(d2[i - 1], d2[i - 2] + sticker[i]);
         }
-        return Math.max(d1[n - 2], Math.max(d2[n - 2], d2[n - 3] + sticker[n - 1]));
+        d1[n - 1] = Math.max(d1[n - 2], d1[n - 3]);
+        d2[n - 1] = Math.max(d2[n - 2], d2[n - 3] + sticker[n - 1]);
+        return Math.max(d1[n - 1], d2[n - 1]);
     }
 }
