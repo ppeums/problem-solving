df24d62854a39ed66d097a04f4e783db8c2f5d2b
ppeums <dlrlqma006@naver.com>
2021-07-06 20:35:51 +0900

    Resolve pro42862 체육복 (2021-07-06)

--- Programmers/pro42862_체육복.java
+++ Programmers/pro42862_체육복.java
@@ -3,28 +3,25 @@ import java.util.Arrays;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-        int[] stu = new int[n + 1];
-        Arrays.fill(stu, 1);
-        for (int i = 0; i < lost.length; i++) {
-            stu[lost[i]]--;
+        int[] clothes = new int[n + 2];
+        Arrays.fill(clothes, 1);
+        for (int re : reserve) {
+            clothes[re]++;
         }
-        for (int i = 0; i < reserve.length; i++) {
-            stu[reserve[i]]++;
-        }
-        for (int i = 1; i <= n; i++) {
-            if (stu[i] == 2) {
-                if (i - 1 >= 1 && stu[i - 1] == 0) {
-                    stu[i - 1]++;
-                    stu[i]--;
-                } else if (i + 1 <= n && stu[i + 1] == 0) {
-                    stu[i + 1]++;
-                    stu[i]--;
-                }
-            }
+        for (int lo : lost) {
+            clothes[lo]--;
         }
         for (int i = 1; i <= n; i++) {
-            if (stu[i] > 0) {
+            if (clothes[i] > 0) {
                 answer++;
+            } else {
+                if (clothes[i - 1] > 1) {
+                    clothes[i - 1]--;
+                    answer++;
+                } else if (clothes[i + 1] > 1) {
+                    clothes[i + 1]--;
+                    answer++;
+                }
             }
         }
         return answer;
