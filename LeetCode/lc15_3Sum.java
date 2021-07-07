170640faf4eadda2cff81cc7aeb8f5f7806f5e0a
ppeums <dlrlqma006@naver.com>
2021-07-07 23:34:33 +0900

    Resolve lc15 3Sum (2021-07-07)

--- LeetCode/lc15_3Sum.java
+++ LeetCode/lc15_3Sum.java
@@ -2,27 +2,26 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> answer = new ArrayList<>();
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            int left = i + 1;
-            int right = nums.length - 1;
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum > 0) {
-                    right--;
-                } else if (sum < 0) {
-                    left++;
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                if (nums[i] + nums[j] + nums[k] < 0) {
+                    j++;
+                } else if (nums[i] + nums[j] + nums[k] > 0) {
+                    k--;
                 } else {
-                    answer.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    left++;
-                    right--;
-                    while (left < right && nums[left] == nums[left - 1]) {
-                        left++;
+                    answer.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        j++;
                     }
-                    while (left < right && nums[right] == nums[right + 1]) {
-                        right--;
+                    while (j < k && nums[k] == nums[k + 1]) {
+                        k--;
                     }
                 }
             }
