38a2a3d75885c679deb77a4fcb4aca5c2f127f92
ppeums <dlrlqma006@naver.com>
2021-07-09 22:36:05 +0900

    Resolve pro42578 위장 (2021-07-09)

--- Programmers/pro42578_위장.java
+++ Programmers/pro42578_위장.java
@@ -2,27 +2,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-    static int answer = 0, idx = 0;
-    static int[] arr;
-
     public int solution(String[][] clothes) {
-        Map<String, Integer> m = new HashMap<>();
-        for (int i = 0; i < clothes.length; i++)
-            m.put(clothes[i][1], m.getOrDefault(clothes[i][1], 0) + 1);
-        arr = new int[m.size()];
-        for (Integer i : m.values())
-            arr[idx++] = i;
-        DFS(0, 0);
-        return answer;
-    }
-
-    static void DFS(int cnt, int start) {
-        if (cnt == idx) return;
-        for (int i = start; i < arr.length; i++) {
-            for (int j = 0; j < arr[i]; j++) {
-                answer++;
-                DFS(cnt + 1, i + 1);
-            }
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        for (String[] cloth : clothes) {
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
         }
+        for (String key : map.keySet()) {
+            answer *= map.get(key) + 1;
+        }
+        return answer - 1;
     }
 }
