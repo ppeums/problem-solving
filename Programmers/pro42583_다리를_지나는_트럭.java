895dc0c43ae714a7df48c37f3a2b0f7665704170
ppeums <dlrlqma006@naver.com>
2021-07-17 23:00:19 +0900

    Resolve: pro42583 다리를 지나는 트럭 (2021-07-17)

--- Programmers/pro42583_다리를_지나는_트럭.java
+++ Programmers/pro42583_다리를_지나는_트럭.java
@@ -4,19 +4,30 @@ import java.util.Queue;
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
+        Queue<Integer> truck = new LinkedList<>();
         Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < bridge_length; i++)
+        for (int t : truck_weights) {
+            truck.add(t);
+        }
+        for (int i = 0; i < bridge_length; i++) {
             q.add(0);
-        int sum = 0, idx = 0;
+        }
+        int current = 0;
         while (true) {
             answer++;
-            sum -= q.poll();
-            if (idx == truck_weights.length && sum == 0)
+            current -= q.poll();
+            if (truck.isEmpty() && current == 0) {
                 break;
-            if (idx < truck_weights.length && sum + truck_weights[idx] <= weight) {
-                q.add(truck_weights[idx]);
-                sum += truck_weights[idx++];
-            } else q.add(0);
+            }
+            if (!truck.isEmpty()) {
+                int peek = truck.peek();
+                if (peek <= weight - current) {
+                    q.add(peek);
+                    current += truck.poll();
+                } else {
+                    q.add(0);
+                }
+            }
         }
         return answer;
     }
