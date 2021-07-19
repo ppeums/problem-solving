79b77d9ef116f2c2e94fd2ab9be2cbc1b2813a26
ppeums <dlrlqma006@naver.com>
2021-07-19 22:21:09 +0900

    Resolve: pro42626 더 맵게 (2021-07-19)

--- Programmers/pro42626_더_맵게.java
+++ Programmers/pro42626_더_맵게.java
@@ -4,11 +4,16 @@ class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int i : scoville)
-            pq.add(i);
+        for (int i = 0; i < scoville.length; i++) {
+            pq.add(scoville[i]);
+        }
         while (true) {
-            if (pq.peek() >= K) break;
-            if (pq.size() < 2) return -1;
+            if (pq.peek() >= K) {
+                break;
+            } else if (pq.size() < 2) {
+                answer = -1;
+                break;
+            }
             int first = pq.poll();
             int second = pq.poll();
             pq.add(first + second * 2);
