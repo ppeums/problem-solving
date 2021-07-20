021cc4008c17d3f1a5bf0839abe41da2a60dcbca
ppeums <dlrlqma006@naver.com>
2021-07-20 21:46:34 +0900

    Resolve: pro42746 가장 큰 수 (2021-07-20)

--- Programmers/pro42746_가장_큰_수.java
+++ Programmers/pro42746_가장_큰_수.java
@@ -5,23 +5,21 @@ class Solution {
     public String solution(int[] numbers) {
         String answer = "";
         String[] str = new String[numbers.length];
-        for (int i = 0; i < numbers.length; i++)
+        for (int i = 0; i < numbers.length; i++) {
             str[i] = Integer.toString(numbers[i]);
+        }
         Arrays.sort(str, new Comparator<String>() {
             @Override
-            public int compare(String s1, String s2) {
-                if (Integer.parseInt(s1 + s2) < Integer.parseInt(s2 + s1))
-                    return 1;
-                else if (Integer.parseInt(s1 + s2) > Integer.parseInt(s2 + s1))
-                    return -1;
-                else
-                    return 0;
+            public int compare(String o1, String o2) {
+                return (o2 + o1).compareTo(o1 + o2);
             }
         });
-        for (int i = 0; i < str.length; i++)
+        for (int i = 0; i < str.length; i++) {
             answer += str[i];
-        while (!answer.equals("0") && answer.startsWith("0"))
+        }
+        while ((answer.length() > 1) && (answer.startsWith("0"))) {
             answer = answer.substring(1, answer.length());
+        }
         return answer;
     }
 }
