b391af501ebe9c3f2b48f3b3f5e69e45b7ddefa8
ppeums <dlrlqma006@naver.com>
2021-07-20 21:46:59 +0900

    Resolve: pro42747 H-Index (2021-07-20)

--- Programmers/pro42747_H-Index.java
+++ Programmers/pro42747_H-Index.java
@@ -4,14 +4,15 @@ class Solution {
     public int solution(int[] citations) {
         int answer = 0;
         Arrays.sort(citations);
-        for (int i = citations[citations.length - 1]; i >= 0; i--) {
+        for (int h = citations[citations.length - 1]; h >= 0; h--) {
             int cnt = 0;
-            for (int j = 0; j < citations.length; j++) {
-                if (citations[j] >= i)
+            for (int i = citations.length - 1; i >= 0; i--) {
+                if (citations[i] >= h) {
                     cnt++;
+                }
             }
-            if (cnt >= i) {
-                answer = i;
+            if (cnt >= h) {
+                answer = h;
                 break;
             }
         }
