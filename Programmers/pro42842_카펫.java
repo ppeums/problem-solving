192cd1bcf7525fa8029bb601f21c2695b5eef507
ppeums <dlrlqma006@naver.com>
2021-07-21 22:38:16 +0900

    Resolve: pro42842 카펫 (2021-07-21)

--- Programmers/pro42842_카펫.java
+++ Programmers/pro42842_카펫.java
@@ -1,12 +1,16 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int half = (brown - 4) / 2;
-        for (int i = 1; i <= yellow; i++) {
-            if (yellow % i == 0 && i + (yellow / i) == half) {
-                answer[0] = yellow / i + 2;
-                answer[1] = i + 2;
-                break;
+        int total = brown + yellow;
+        for (int i = 1; i < total; i++) {
+            if (total % i == 0) {
+                int row = i;
+                int col = total / i;
+                if ((row - 2) * (col - 2) == yellow) {
+                    answer[0] = col;
+                    answer[1] = row;
+                    break;
+                }
             }
         }
         return answer;
