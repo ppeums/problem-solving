e91094788ced1029b110cbd4b958d4259de64022
ppeums <dlrlqma006@naver.com>
2021-07-24 22:38:50 +0900

    Resolve: pro43165 타겟 넘버 (2021-07-24)

--- Programmers/pro43165_타겟_넘버.java
+++ Programmers/pro43165_타겟_넘버.java
@@ -1,21 +1,21 @@
 class Solution {
-    static int[] input;
-    static int key, answer = 0;
+    int[] input;
+    int answer;
 
     public int solution(int[] numbers, int target) {
         input = numbers;
-        key = target;
-        DFS(0, 0);
+        findTarget(0, 0, target);
         return answer;
     }
 
-    static void DFS(int start, int sum) {
-        if (start == input.length) {
-            if (sum == key)
+    public void findTarget(int cnt, int sum, int target) {
+        if (cnt == input.length) {
+            if (sum == target) {
                 answer++;
+            }
             return;
         }
-        DFS(start + 1, sum + input[start]);
-        DFS(start + 1, sum - input[start]);
+        findTarget(cnt + 1, sum + input[cnt], target);
+        findTarget(cnt + 1, sum - input[cnt], target);
     }
 }
