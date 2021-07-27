e18d4690e70697d97c2b9309877a1636a1999514
ppeums <dlrlqma006@naver.com>
2021-07-27 11:19:32 +0900

    Resolve: pro42584 주식가격 (2021-07-27)

--- Programmers/pro42584_주식가격.java
+++ Programmers/pro42584_주식가격.java
@@ -1,8 +1,9 @@
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-        for (int i = 0; i < prices.length; i++) {
-            for (int j = i + 1; j < prices.length; j++) {
+        int len = prices.length;
+        int[] answer = new int[len];
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
                 answer[i]++;
                 if (prices[i] > prices[j]) {
                     break;
