90425c14ace9906b8f6350363145e35211221443
ppeums <dlrlqma006@naver.com>
2021-07-29 12:24:23 +0900

    Resolve: pro42628 이중우선순위큐 (2021-07-29)

--- Programmers/pro42628_이중우선순위큐.java
+++ Programmers/pro42628_이중우선순위큐.java
@@ -5,22 +5,49 @@ class Solution {
     public int[] solution(String[] operations) {
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
         for (String operation : operations) {
             char op = operation.charAt(0);
             int num = Integer.parseInt(operation.substring(2));
+
             if (op == 'I') {
-                maxHeap.add(num);
                 minHeap.add(num);
-            } else if (!maxHeap.isEmpty()) {
+                maxHeap.add(num);
+            } else {
                 if (num == 1) {
-                    int max = maxHeap.poll();
-                    minHeap.remove(max);
+                    if (!maxHeap.isEmpty()) {
+                        if (maxHeap.peek() >= minHeap.peek()) {
+                            maxHeap.poll();
+                        }
+                    }
                 } else {
-                    int min = minHeap.poll();
-                    maxHeap.remove(min);
+                    if (!minHeap.isEmpty()) {
+                        if (maxHeap.peek() >= minHeap.peek()) {
+                            minHeap.poll();
+                        }
+                    }
                 }
             }
+
+            if (!minHeap.isEmpty() && !maxHeap.isEmpty() && (minHeap.peek() > maxHeap.peek())) {
+                maxHeap.clear();
+                minHeap.clear();
+            }
         }
-        return (maxHeap.isEmpty()) ? new int[]{0, 0} : new int[]{maxHeap.poll(), minHeap.poll()};
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        while (!minHeap.isEmpty() && !maxHeap.isEmpty() && (minHeap.peek() <= maxHeap.peek())) {
+            max = Math.max(max, maxHeap.poll());
+            min = Math.min(min, minHeap.poll());
+        }
+
+        if ((max == Integer.MIN_VALUE) && (min == Integer.MAX_VALUE)) {
+            max = 0;
+            min = 0;
+        }
+
+        return new int[]{max, min};
     }
 }
