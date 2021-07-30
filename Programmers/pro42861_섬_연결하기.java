388ff6a5927336d9ecdb45c3d6adee94a47d287a
ppeums <dlrlqma006@naver.com>
2021-07-30 11:59:22 +0900

    Resolve: pro42861 섬 연결하기 (2021-07-30)

--- Programmers/pro42861_섬_연결하기.java
+++ Programmers/pro42861_섬_연결하기.java
@@ -1,39 +1,40 @@
 import java.util.Arrays;
 
 class Solution {
-    static int[] parent;
+    int[] parent;
 
     public int solution(int n, int[][] costs) {
         int answer = 0;
         parent = new int[n];
+
         Arrays.sort(costs, (o1, o2) -> Integer.compare(o1[2], o2[2]));
-        for (int[] cost : costs) {
-            Arrays.sort(cost, 0, 2);
-        }
-        init();
+
+        init(n);
+
         for (int[] cost : costs) {
             if (find(cost[0]) != find(cost[1])) {
                 union(cost[0], cost[1]);
                 answer += cost[2];
             }
         }
+
         return answer;
     }
 
-    static void init() {
-        for (int i = 0; i < parent.length; i++) {
+    public void init(int n) {
+        for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
     }
 
-    static int find(int x) {
-        if (x == parent[x]) {
+    public int find(int x) {
+        if (parent[x] == x) {
             return x;
         }
         return find(parent[x]);
     }
 
-    static void union(int x, int y) {
+    public void union(int x, int y) {
         x = find(x);
         y = find(y);
         parent[y] = x;
