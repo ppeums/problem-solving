0493869a4bf23c0e2a10b335309c26e9070ef5b3
ppeums <dlrlqma006@naver.com>
2021-08-05 14:27:39 +0900

    Resolve: pro43105 정수 삼각형 (2021-08-05)

--- Programmers/pro43105_정수_삼각형.java
+++ Programmers/pro43105_정수_삼각형.java
@@ -1,23 +1,24 @@
 class Solution {
     public int solution(int[][] triangle) {
-        int answer = 0, N = triangle.length;
-        int[][] d = new int[N][N];
-        d[0][0] = triangle[0][0];
-        for (int i = 1; i < N; i++) {
-            for (int j = 0; j < i + 1; j++) {
+        int answer = 0;
+        int[][] dp = new int[triangle.length][triangle.length];
+
+        dp[0][0] = triangle[0][0];
+
+        for (int i = 1; i < triangle.length; i++) {
+            for (int j = 0; j < triangle[i].length; j++) {
                 if (j == 0) {
-                    d[i][j] = d[i - 1][j];
-                } else if (j == i) {
-                    d[i][j] = d[i - 1][j - 1];
+                    dp[i][j] = triangle[i][j] + dp[i - 1][j];
+                } else if (j == triangle[i].length - 1) {
+                    dp[i][j] = triangle[i][j] + dp[i - 1][j - 1];
                 } else {
-                    d[i][j] = Math.max(d[i - 1][j], d[i - 1][j - 1]);
+                    dp[i][j] = triangle[i][j] + Math.max(dp[i - 1][j], dp[i - 1][j - 1]);
                 }
-                d[i][j] += triangle[i][j];
+
+                answer = Math.max(answer, dp[i][j]);
             }
         }
-        for (int i = 0; i < N; i++) {
-            answer = Math.max(answer, d[N - 1][i]);
-        }
+
         return answer;
     }
 }
