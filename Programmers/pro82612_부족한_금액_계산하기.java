5e40121dab0a6b085bbfcb21ae008b4698f618aa
ppeums <dlrlqma006@naver.com>
2021-08-08 16:12:01 +0900

    Solve: pro82612 부족한 금액 계산하기 (2021-08-08)

---
+++ Programmers/pro82612_부족한_금액_계산하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long sum = 0;
+
+        for (int i = 1; i <= count; i++) {
+            sum += price * i;
+        }
+
+        return (sum > money) ? sum - money : 0;
+    }
+}
