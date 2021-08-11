1ce97a13043f2db631282fc4bd9eecf984b7065b
ppeums <dlrlqma006@naver.com>
2021-08-11 13:20:09 +0900

    Resolve: pro43164 여행경로 (2021-08-11)

--- Programmers/pro43164_여행경로.java
+++ Programmers/pro43164_여행경로.java
@@ -1,36 +1,43 @@
-import java.util.ArrayList;
-import java.util.Collections;
-
 class Solution {
-    static String[][] input;
-    static boolean[] visited;
-    static ArrayList<String> answer;
+    String[][] input;
+    boolean[] used;
+    String answer;
+    int N;
 
     public String[] solution(String[][] tickets) {
+        answer = "";
         input = tickets;
-        visited = new boolean[tickets.length];
-        answer = new ArrayList<>();
-        for (int i = 0; i < tickets.length; i++) {
-            if (tickets[i][0].equals("ICN")) {
-                visited[i] = true;
-                dfs(tickets[i][1], tickets[i][0] + "," + tickets[i][1], 1);
-                visited[i] = false;
+        N = tickets.length;
+        used = new boolean[N];
+
+        for (int i = 0; i < N; i++) {
+            if (!used[i] && tickets[i][0].equals("ICN")) {
+                String route = tickets[i][0] + " " + tickets[i][1];
+                used[i] = true;
+                dfs(1, N, tickets[i][1], route);
+                used[i] = false;
             }
         }
-        Collections.sort(answer);
-        return answer.get(0).split(",");
+
+        return answer.split(" ");
     }
 
-    static void dfs(String before, String route, int ticketCount) {
-        if (ticketCount == input.length) {
-            answer.add(route);
+    public void dfs(int cnt, int target, String prev, String route) {
+        if (cnt == target) {
+            if (answer.equals("")) {
+                answer = route;
+            } else {
+                answer = (answer.compareTo(route) > 0) ? route : answer;
+            }
             return;
         }
-        for (int i = 0; i < input.length; i++) {
-            if (!visited[i] && input[i][0].equals(before)) {
-                visited[i] = true;
-                dfs(input[i][1], route + "," + input[i][1], ticketCount + 1);
-                visited[i] = false;
+
+        for (int i = 0; i < N; i++) {
+            if (!used[i] && input[i][0].equals(prev)) {
+                String newRoute = route + " " + input[i][1];
+                used[i] = true;
+                dfs(cnt + 1, target, input[i][1], newRoute);
+                used[i] = false;
             }
         }
     }
