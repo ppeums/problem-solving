8d0cb0d3925ce5f8c09ccb0c2fbc663694b7f526
ppeums <dlrlqma006@naver.com>
2021-08-14 16:27:44 +0900

    Resolve: pro49191 순위 (2021-08-14)

--- Programmers/pro49191_순위.java
+++ Programmers/pro49191_순위.java
@@ -4,35 +4,43 @@ import java.util.Set;
 class Solution {
     public int solution(int n, int[][] results) {
         int answer = 0;
-        Set<Integer>[] winner = new Set[n + 1];
-        Set<Integer>[] loser = new Set[n + 1];
+        Set<Integer>[] win = new Set[n + 1];
+        Set<Integer>[] lose = new Set[n + 1];
+
         for (int i = 1; i <= n; i++) {
-            winner[i] = new HashSet<>();
-            loser[i] = new HashSet<>();
+            win[i] = new HashSet<>();
+            lose[i] = new HashSet<>();
         }
-        for (int i = 0; i < results.length; i++) {
-            loser[results[i][0]].add(results[i][1]);
-            winner[results[i][1]].add(results[i][0]);
+
+        for (int[] r : results) {
+            win[r[1]].add(r[0]);
+            lose[r[0]].add(r[1]);
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j <= n; j++) {
+
+        for (int t = 0; t < n; t++) {
+            for (int i = 1; i <= n; i++) {
                 Set<Integer> winSet = new HashSet<>();
-                for (Integer win : winner[j]) {
-                    winSet.addAll(winner[win]);
-                }
-                winner[j].addAll(winSet);
                 Set<Integer> loseSet = new HashSet<>();
-                for (Integer lose : loser[j]) {
-                    loseSet.addAll(loser[lose]);
+
+                for (int w : win[i]) {
+                    winSet.addAll(win[w]);
                 }
-                loser[j].addAll(loseSet);
+
+                for (int l : lose[i]) {
+                    loseSet.addAll(lose[l]);
+                }
+
+                win[i].addAll(winSet);
+                lose[i].addAll(loseSet);
             }
         }
+
         for (int i = 1; i <= n; i++) {
-            if (winner[i].size() + loser[i].size() == n - 1) {
+            if (win[i].size() + lose[i].size() == n - 1) {
                 answer++;
             }
         }
+
         return answer;
     }
 }
