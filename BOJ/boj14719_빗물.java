2846bafd1dc6b66ccedf31d60b2be0c124ef6e11
ppeums <dlrlqma006@naver.com>
2021-08-17 16:07:19 +0900

    Resolve: boj14719 빗물 (2021-08-17)

--- BOJ/boj14719_빗물.java
+++ BOJ/boj14719_빗물.java
@@ -2,30 +2,42 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int answer = 0;
         int H = Integer.parseInt(st.nextToken());
         int W = Integer.parseInt(st.nextToken());
         int[] block = new int[W];
+
         st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < W; i++) {
             block[i] = Integer.parseInt(st.nextToken());
         }
-        int answer = 0;
+
         for (int i = 1; i < W - 1; i++) {
-            int left = 0, right = 0;
+            int rain = 0;
+            int leftMax = 0;
+            int rightMax = 0;
+
             for (int j = 0; j < i; j++) {
-                left = Math.max(left, block[j]);
+                leftMax = Math.max(leftMax, block[j]);
             }
+
             for (int j = i + 1; j < W; j++) {
-                right = Math.max(right, block[j]);
+                rightMax = Math.max(rightMax, block[j]);
             }
-            int diff = Math.min(left, right) - block[i];
-            answer += (diff > 0) ? diff : 0;
+
+            rain = Math.min(leftMax, rightMax);
+
+            if (rain > block[i]) {
+                answer += rain - block[i];
+            }
         }
+
         bw.write(answer + "\n");
         bw.flush();
         bw.close();
