259e6cb388919066598aa6b49b41727ff878ee5c
ppeums <dlrlqma006@naver.com>
2021-09-12 20:41:16 +0900

    Resolve: pro42577 전화번호 목록 (2021-09-12)

--- Programmers/pro42577_전화번호_목록.java
+++ Programmers/pro42577_전화번호_목록.java
@@ -2,14 +2,14 @@ import java.util.Arrays;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
         Arrays.sort(phone_book);
+
         for (int i = 1; i < phone_book.length; i++) {
             if (phone_book[i].startsWith(phone_book[i - 1])) {
-                answer = false;
-                break;
+                return false;
             }
         }
-        return answer;
+
+        return true;
     }
 }
